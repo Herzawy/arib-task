9fc8035e7a7466457daa99bacea0f6eac7cf3857
Herzawy <43451453+Herzawy@users.noreply.github.com>
2024-11-12 18:56:22 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       if (env('APP_ENV') === 'production') {
+        \URL::forceScheme('https');
+        }
     }
 }
